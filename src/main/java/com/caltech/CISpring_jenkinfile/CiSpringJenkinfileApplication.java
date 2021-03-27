66087e4150bdef3bf3e4f721d064cb75944eebcf
student <ram.jmeka@gmail.com>
2021-03-27 17:07:13 +0000

    CI pIpeline Jenkinsfile

---
+++ src/main/java/com/caltech/CISpring_jenkinfile/CiSpringJenkinfileApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring_jenkinfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringJenkinfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringJenkinfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/CISpring_jenkinfile/CiSpringJenkinfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring_jenkinfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringJenkinfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
